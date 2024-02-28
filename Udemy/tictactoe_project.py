561c8274dc9e6e7d288a54e80db5b82f93151949
ChadwickyN <57159358+ChadwickyN@users.noreply.github.com>
2024-02-28 14:23:42 -0700

    Tic tac toe and learning

---
+++ Udemy/tictactoe_project.py
@@ -0,0 +1,22 @@
+#tictactoe project
+
+# USER INTERFACE
+
+begin_game = input('Hell! would you like to play a game of tic tac toe? (Y/N) ')
+
+if begin_game == 'Y':
+    print("lets go!")
+elif begin_game == 'N':
+    print('k bye')
+else:
+    print('Try again')
+
+def ask_player_to_pick():
+    print(board)
+
+# def print_board(row1,row2,row3):
+#     print(row1)
+#     print(row2)
+#     print(row3)
+#     pass
+
